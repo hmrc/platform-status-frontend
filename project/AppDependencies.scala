f67d131002bd30007dd6a7c935a6cfbdeadfcd4d
Muhammed Ahmed <ma3574@users.noreply.github.com>
2025-01-16 09:08:24 +0000

    Remove trailing comma

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "10.12.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "org.typelevel"     %% "cats-core"                  % "2.12.0",
-    "org.mongodb.scala" %% "mongo-scala-driver"         % "5.1.1" cross CrossVersion.for3Use2_13,
+    "org.mongodb.scala" %% "mongo-scala-driver"         % "5.1.1" cross CrossVersion.for3Use2_13
   )
 
   val test: Seq[ModuleID] = Seq(
